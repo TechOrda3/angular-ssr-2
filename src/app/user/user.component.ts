b15024c95cac76b440934df9bb6743d9bd815dc1
Нуртилек Кошкар <nurtylek98@gmail.com>
2023-04-06 21:12:45 +0600

    add meta tags

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
 import {switchMap} from 'rxjs';
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-user',
@@ -13,13 +14,32 @@ import {switchMap} from 'rxjs';
 })
 export class UserComponent implements OnInit {
   private http = inject(HttpClient);
-  user = inject(ActivatedRoute).paramMap.pipe(
-    switchMap(paramMap => this.http.get<any>('https://jsonplaceholder.typicode.com/users/' + paramMap.get('id')))
-  );
+  private meta = inject(Meta);
+  private title = inject(Title);
+  private route = inject(ActivatedRoute);
+
+  user: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.http.get<any>('https://jsonplaceholder.typicode.com/users/' + id).subscribe({
+      next: user => {
+        this.user = user;
+        this.title.setTitle(user.username);
+        this.meta.addTags([
+          {
+            property: 'description',
+            content: 'Username is' + user.username
+          },
+          {
+            property: 'og:description',
+            content: 'Username is' + user.username
+          }
+        ])
+      }
+    })
   }
 
 }
